3c64d32a8e0611321a524ec1b037f974437f0904
Dhruv Upadhyay <46966691+Dhruv0Upadhyay@users.noreply.github.com>
2023-11-18 20:31:43 +0530

    Update testbench.v

--- Day13/testbench.v
+++ Day13/testbench.v
@@ -2,10 +2,10 @@ module tb;
   reg [3:0] A, B;
   wire A_grt_B, A_less_B, A_eq_B;
   
-  comparator comp(A, B, A_grt_B, A_less_B, A_eq_B);
+  comparator dut(A, B, A_grt_B, A_less_B, A_eq_B);
   
   initial begin
-    $monitor("A = %0h, B = %0h -> A_grt_B = %0b, A_less_B = %0b, A_eq_B = %0b", A, B, A_grt_B, A_less_B, A_eq_B);
+    $monitor("A = %0h, B = %0h  A_grt_B = %0b, A_less_B = %0b, A_eq_B = %0b", A, B, A_grt_B, A_less_B, A_eq_B);
     repeat(10) begin
       A=$random; B=$random; #1;
     end
